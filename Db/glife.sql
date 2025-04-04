d579c87e4f2cc98a46afe99aa49a65857be6b288
Amitkalsariya <156776217+Amitkalsariya@users.noreply.github.com>
2025-04-04 17:08:39 +0530

    Updated DB Added

--- Db/glife.sql
+++ Db/glife.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2025 at 06:18 AM
+-- Generation Time: Apr 04, 2025 at 01:37 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -38,7 +38,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `email`, `password`) VALUES
-(1, 'ami@gmail.com', '2113');
+(1, 'ami@gmail.com', '2113'),
+(2, 'group13@gmail.com', '1234');
 
 -- --------------------------------------------------------
 
@@ -66,9 +67,7 @@ INSERT INTO `cart` (`c_id`, `p_id`, `p_price`, `u_id`, `qty`, `status`) VALUES
 (49, 5, 1200, 21, 6, 1),
 (50, 6, 400, 21, 3, 1),
 (51, 9, 800, 21, 8, 1),
-(52, 7, 500, 22, 1, 1),
-(79, 6, 500, 20, 1, 0),
-(80, 5, 1200, 20, 1, 0);
+(52, 7, 500, 22, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -104,7 +103,21 @@ INSERT INTO `checkout` (`id`, `firstname`, `lastname`, `emailid`, `phonenumber`,
 (16, 'gtdg', 'safc', '', 0, '', '', '', '', 'KhXWy2sMSlQauhd1'),
 (17, 'Nemu', 'asdad', 's@gmaqil.com', 2147483647, 'Ambika', 'Surat', 'Gujarat', 'sdgsd', 'pay_QEATRQXgvVk9gL'),
 (18, 'sada', 'dsfsfs', 's@gmaqil.com', 2147483647, 'Ambika', 'Surat', 'Gujarat', '395004', 'pay_QEAZXwlnTBfry9'),
-(19, 'Jack', 'Reacher', 'reacher@gmail.com', 2147483647, 'Surat', 'Surat', 'Gujarat', '395006', 'pay_QEAsMgqzc7o8Hs');
+(19, 'Jack', 'Reacher', 'reacher@gmail.com', 2147483647, 'Surat', 'Surat', 'Gujarat', '395006', 'pay_QEAsMgqzc7o8Hs'),
+(20, '', '', '', 0, '', '', '', '', 'COD-1743666963'),
+(21, 'etrf', 'tsrdg', 's@gmaqil.com', 2147483647, 'aSD', 'asd', 'adS', 'C', 'COD-1743667494'),
+(22, '', '', '', 0, '', '', '', '', 'COD'),
+(23, 'Nemu', 'asdad', 'morty@gmail.com', 2147483647, 'aSD', 'Surat', 'Gujarat', '395006', 'COD'),
+(24, '', '', '', 0, '', '', '', '', 'COD'),
+(25, '', '', '', 0, '', '', '', '', 'pay_QEWK1hkaQad4ic'),
+(26, 'Muinna', 'MBBS', 'munna@gmail.com', 2147483647, 'Mumbai', 'Surat', 'Suraty', '85006', 'COD'),
+(27, 'Muinna', 'MBBS', 'munna@gmail.com', 2147483647, 'Mumbai', 'Surat', 'Suraty', '85006', 'pay_QEWNKqFqJNGLK7'),
+(28, 'BHurii', 'MBBS', 'munna@gmail.com', 2147483647, 'Mumbai', 'Surat', 'Suraty', '85006', 'pay_QEWStbxTwxTnxI'),
+(29, 'Jwtho', 'Gada', 'jetha@gmail.com', 2147483647, 'Mumbai', 'Surat', 'Guj', '390055', 'COD'),
+(30, 'Jwtho', 'Gada', 'jetha@gmail.com', 2147483647, 'Mumbai', 'Surat', 'Guj', '390055', 'pay_QEWX4DFmpTCVPO'),
+(31, 'Jwtho', 'Gada', 'jetha@gmail.com', 2147483647, 'Mumbai', 'Surat', 'Guj', '390055', 'COD_766032'),
+(32, '', '', '', 0, '', '', '', '', 'COD_67700'),
+(33, 'Jack', 'Reacher', 'jack@gmail.com', 2147483647, 'London', 'L', 'L', '890065', 'COD_513880');
 
 -- --------------------------------------------------------
 
@@ -129,7 +142,8 @@ INSERT INTO `comment` (`C_id`, `Post_id`, `R_id`, `Text`, `Date`, `Time`) VALUES
 (24, 30, 19, 'best colour', '2024-09-27', '09:50:48'),
 (25, 29, 19, 'dgtrt ret rt', '2024-09-27', '09:52:41'),
 (26, 29, 19, 'esffgg', '2024-09-27', '09:52:56'),
-(29, 31, 20, 'Improve Reponse Time', '2025-03-03', '18:05:29');
+(29, 31, 20, 'Improve Reponse Time', '2025-03-03', '18:05:29'),
+(30, 32, 34, 'Nurture nature, embrace green – a better life begi', '2025-04-03', '15:01:14');
 
 -- --------------------------------------------------------
 
@@ -149,7 +163,8 @@ CREATE TABLE `faq` (
 --
 
 INSERT INTO `faq` (`fq_id`, `fq_type`, `Question`, `Answer`) VALUES
-(36, 'order related', 'What are the 4 basic parts of plants list ?', 'roots, stems, leaves, flowers ,ledders');
+(38, 'order related', 'Which Payment Method Will be Work Smoothly ?', 'Razorpay Is The One of The Best Online P'),
+(39, 'plant related', 'Which Plants are selling on our platform', 'Seeds, Magical Seeds , Plants and etc');
 
 -- --------------------------------------------------------
 
@@ -179,7 +194,8 @@ INSERT INTO `plant_details` (`Plant_id`, `Plant_image`, `Type_name`, `Date`, `Ti
 (11, '22131727434825h4.jpg', 'Home Design', '2024-09-27', '16:30:25', 0, 'relling decor'),
 (12, '51551727434924g4.jfif', 'Garden', '2024-09-27', '16:32:04', 0, 'red flower outdoor'),
 (13, '51481727434949g2.jfif', 'Garden', '2024-09-27', '16:32:29', 0, 'green flower'),
-(16, '3648174099055118971727357367f2.jfif', 'Garden', '2025-03-03', '13:59:11', 0, 'Delicious');
+(16, '3648174099055118971727357367f2.jfif', 'Garden', '2025-03-03', '13:59:11', 0, 'Delicious'),
+(17, '62451743673951459617273576751.jfif', 'Office Design', '2025-04-03', '15:22:31', 0, 'Best Design Decoration');
 
 -- --------------------------------------------------------
 
@@ -224,7 +240,7 @@ CREATE TABLE `post` (
 INSERT INTO `post` (`Post_id`, `R_id`, `Post_image`, `Caption`, `Date`, `Time`) VALUES
 (29, 19, '123.png', 'best plant', '2024-09-27', '09:30:33'),
 (30, 19, '4.jfif', 'beautiful flower', '2024-09-27', '09:33:09'),
-(31, 20, 'pexels-photo-3685647.jpeg', 'Best Service', '2025-03-03', '16:42:13');
+(32, 34, 'Group-no_13.jpeg', 'This project is Developed By Gruop NO 13', '2025-04-03', '14:59:53');
 
 -- --------------------------------------------------------
 
@@ -258,7 +274,8 @@ INSERT INTO `product` (`Prod_id`, `Prod_name`, `Prod_image`, `Prod_description`,
 (9, 'Merigolds', 'diy-organic-candle.jpg', 'The bloom itself symbolizes beautys', 900, 5, '2024-09-27', '16:03:51', 1, NULL),
 (10, 'Lily', '12881727433313lily.jpg', 'The inhalation of fragrance from certain', 450, 8, '2024-09-27', '16:05:13', 2, NULL),
 (11, 'designer port', '67601727433869pot.jpg', 'best design', 200, 2, '2024-09-27', '16:14:29', 3, NULL),
-(15, 'Fsn', '36771743394506pexels-photo-3685647.jpeg', 'This is Realastic', 3000, 2, '2025-03-31', '09:45:06', 2, NULL);
+(15, 'Fsn', '36771743394506pexels-photo-3685647.jpeg', 'This is Realastic', 3000, 2, '2025-03-31', '09:45:06', 2, NULL),
+(16, 'Magical Seeds', '72251743674103856217272696233.jfif', 'This is Magical Seeds', 500, 1, '2025-04-03', '15:25:03', 4, NULL);
 
 -- --------------------------------------------------------
 
@@ -281,7 +298,8 @@ INSERT INTO `product_category` (`Pc_id`, `Pc_name`, `Pc_description`) VALUES
 (2, 'Plant', 'This is a Plant Section.'),
 (3, 'Pots', 'This is a Pots Section.'),
 (4, 'Seeds', 'This is a Seeds Section.                '),
-(5, 'Trees', 'This is a Trees Section                 ');
+(5, 'Trees', 'This is a Trees Section                 '),
+(10, 'Magical Beats', 'One of Our Special item ');
 
 -- --------------------------------------------------------
 
@@ -309,9 +327,12 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`R_id`, `name`, `email`, `contact`, `address`, `city`, `date`, `time`, `password`, `image_name`, `image_data`) VALUES
 (20, 'Amit', 'amii@gmail.com', '8576425968', '12321ewe fgdfg', 'Surat', '0000-00-00', '00:00:00', '123', 'profile.png', ''),
-(23, '', '', '', '', NULL, '0000-00-00', '00:00:00', '', '', ''),
-(30, 'Paneer Bhurji', 'reacher@gmail.com', '8745219896', 'Surat', 'Surat', '0000-00-00', '00:00:00', '112233', 'fixer.png', ''),
-(31, 'Amit Kalsariya', 'cdmi@gmail.com', '8745219896', 'Surat', 'Surat', '0000-00-00', '00:00:00', '123456', 'gpt.png', '');
+(23, 'Rutvik', 'r@gmail.com', '8956239856', 'Amroli Surat', 'Surat', '0000-00-00', '00:00:00', '123456', '123456', ''),
+(30, 'Jay', 'j@gmail.com', '8745219896', 'Surat', 'Surat', '0000-00-00', '00:00:00', '112233', 'fixer.png', ''),
+(34, 'BCA SEM 6 PROJECT', 'group13@gmail.com', '9876543211', 'Amroli', 'Surat', '0000-00-00', '00:00:00', '123456', 'Group-no_13.jpeg', ''),
+(35, 'Henil', 'h@gmail.com', '8745219896', 'Surat', 'Surat', '0000-00-00', '00:00:00', '112233', 'fixer.png', ''),
+(36, 'Meet', 'm13@gmail.com', '9876543211', 'Amroli', 'Surat', '0000-00-00', '00:00:00', '123456', 'Group-no_13.jpeg', ''),
+(37, 'Chirag', 'c@gmail.com', '9876543211', 'Amroli', 'Surat', '0000-00-00', '00:00:00', '123456', 'Group-no_13.jpeg', '');
 
 --
 -- Indexes for dumped tables
@@ -399,37 +420,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `checkout`
 --
 ALTER TABLE `checkout`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `C_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `C_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `faq`
 --
 ALTER TABLE `faq`
-  MODIFY `fq_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `fq_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `plant_details`
 --
 ALTER TABLE `plant_details`
-  MODIFY `Plant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `Plant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `plant_type`
@@ -441,25 +462,25 @@ ALTER TABLE `plant_type`
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `Post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `Post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `Prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `Prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `product_category`
 --
 ALTER TABLE `product_category`
-  MODIFY `Pc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `Pc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `R_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `R_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
